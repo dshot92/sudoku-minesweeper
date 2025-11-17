810c7d97fe8af5df5e30c58d47cb1e1641b029bb
dshot92 <dshot92@gmail.com>
2025-11-17 16:13:07 +0100

    feat: PWA works offline

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const withPWA = require("@ducanh2912/next-pwa").default({
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  dest: "public",
+  fallbacks: {
+    document: "/offline",
+  },
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // You can add other Next.js config options here if needed
+};
+
+module.exports = withPWA(nextConfig);
